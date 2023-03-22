ef83915dd9087961749da8ac57795f2d8aef04bb
laryssa_b_oliveira <laryssa_b_oliveira@estudante.sesisenai.org.br>
2023-03-22 10:46:07 -0300

    layout

--- app/src/main/java/com/example/calculadorabug/MainActivity.java
+++ app/src/main/java/com/example/calculadorabug/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.calculadorabug;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText N1;
+    EditText N2;
+    TextView RN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        N1 = findViewById(R.id.N1);
+        N2 = findViewById(R.id.N2);
+        RN = findViewById(R.id.RN);
+    }
+    public void somar(View v){
 
 
     }
